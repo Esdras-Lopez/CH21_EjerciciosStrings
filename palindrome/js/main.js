ab31ebda6da6c4abf06035c71f7b2cc6f83d4bd2
Esdras-Palacios <Esdras.palacios07@Gmail.com>
2022-12-12 16:11:24 -0600

    replace y palindrome

---
+++ palindrome/js/main.js
@@ -0,0 +1,29 @@
+
+let nombre = "Anita lava la tina" ;
+
+//nombre = nombre.toLowerCase;
+
+let nombre2 = nombre.replaceAll(" ", "");
+
+console.log(nombre2)
+
+/*
+function nomb (nombre) {
+
+    let nombreInvertido ="";
+
+    for (let index = nombre.length ; index >= 0 ; index--) {
+        
+     let  letra = nombre.charAt(index);
+
+      nombreInvertido = (nombreInvertido + letra).trim();
+    }
+
+    if (nombreInvertido == nombre){
+        return true
+    }
+ 
+    return false;
+
+} */
+
